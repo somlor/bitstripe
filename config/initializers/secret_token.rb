9376e0ccd29b6939f0d42c913dbcb9b04a98050f
Sean Omlor <somlor@eml.cc>
2014-03-08 22:39:57 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitStripe::Application.config.secret_key_base = '5d048132710ca2aa1d3894400e88a70e91d1e395db5afba2179d64befcd6cf364ae2974e686103d5758720dbab777f6fc660ffc34b1c6bdbd07527e566c8c6b2'
